6c6ac4cd7e99eddeb709d9a93e2c548c0cc8f3a3
HTP8888 <phonght.b21vt339@stu.ptit.edu.vn>
2025-02-24 07:40:06 +0700

    Add files via upload

---
+++ OOP001.java
@@ -0,0 +1,29 @@
+class Dog {
+    private String name;
+    private String breed;
+    private int age;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void printDogDetail() {
+        System.out.println("Name:" + name + "--Breed:" + breed + "--Age:" + age);
+    }
+}
+
+public class OOP001 {
+    public static void main(String[] args) {
+        Dog myDog = new Dog();
+        myDog.setName("Micky");
+        myDog.setBreed("Husky");
+        myDog.setAge(12);
+        myDog.printDogDetail();
+    }
+}
